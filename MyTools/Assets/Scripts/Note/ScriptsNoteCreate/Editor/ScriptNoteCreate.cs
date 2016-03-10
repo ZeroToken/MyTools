d1dde6fff769d6279e48b61e01cba3eb339307cf
ZeroToken <229113130@qq.com>
2016-03-10 21:19:30 +0800

    alter node

---
+++ MyTools/Assets/Scripts/Note/ScriptsNoteCreate/Editor/ScriptNoteCreate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ScriptNoteCreate : UnityEditor.AssetModificationProcessor
+{
+
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        if (path.EndsWith(".cs"))
+        {
+            string allText = File.ReadAllText(path);
+            allText = allText.Replace("#AuthorName#", System.Environment.UserName).Replace("#CreateTime#",
+                System.DateTime.Now.Year + "-" + System.DateTime.Now.Month + "-" + System.DateTime.Now.Day + " " +
+                System.DateTime.Now.Hour + ":" + System.DateTime.Now.Minute + ":" + System.DateTime.Now.Second);
+            File.WriteAllText(path, allText);
+        }
+
+    }
+}
